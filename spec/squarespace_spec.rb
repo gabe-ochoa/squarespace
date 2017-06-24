8b3b339d7ce5d146336a2b8f6bcf6e087312c7f7
Gabe Ochoa <gabeochoa@gmail.com>
2017-06-24 17:26:35 -0400

    Failing test for ss api version

--- spec/squarespace_spec.rb
+++ spec/squarespace_spec.rb
@@ -4,4 +4,9 @@ describe Squarespace do
   it 'has a version number' do
     expect(Squarespace::VERSION).to_not be nil
   end
+
+  it 'has an API version number' do
+    expect(Squarespace::API_VERSION).to be_a Integer
+    expect(Squarespace::API_VERSION).to be > 0
+  end
 end
