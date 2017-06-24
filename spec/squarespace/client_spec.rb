0883f987b6028baa7af043e1a8f4b0172e7a4623
Gabe Ochoa <gabeochoa@gmail.com>
2017-06-24 18:01:49 -0400

    Test for commerce api url

--- spec/squarespace/client_spec.rb
+++ spec/squarespace/client_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe Squarespace::Client do
 
+  test_configuration
+
+  let(:client) { Squarespace::Client.new }
+
+  context 'For the Squarespace Commerce API' do
+    it 'set the commerce api url' do
+      expect(client.commerce_url).to eq 'https://api.squarespace.com/0.1/commerce/orders'
+    end
+  end
 end
